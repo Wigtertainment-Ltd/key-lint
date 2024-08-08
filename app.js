f8ab03499ff71e7cbfa04ca605001b53c1355335
Egor Kin <info@egor-kin.com>
2024-08-08 21:16:30 +0300

    setup

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const { app, BrowserWindow } = require('electron')
-const url = require("url");
-const path = require("path");
+const url = require('url');
+const path = require('path');
 
 let mainWindow
 
